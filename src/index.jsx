de91e31228e86235eb2e2b3e988e732ce445e7ed
Bariq Firjatullah <bariqfirjatullah1803@gmail.com>
2023-10-05 19:44:57 +0700

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import ContactApp from './components/ContactApp.jsx';
+
+import './styles/style.css';
+
+const root = createRoot(document.getElementById('root'));
+root.render(<ContactApp />);
